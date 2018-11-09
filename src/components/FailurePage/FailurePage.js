6adfac00078b242263f4e394ffbdcfd93b22ea74
Caio Incau <caioincau@gmail.com>
2018-11-09 14:49:30 -0200

    Extracting Failure Page

---
+++ src/components/FailurePage/FailurePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CommitItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Error on API, please try again later</h2>
+      </div>
+    )
+  }
+}
+
+
+export default CommitItem
